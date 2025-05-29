cb65f6034974e86ce3d1c51930c7f6f70ffe98e8
s225090522.deakin.edu.au <s225090522.deakin.edu.au>
2025-05-29 14:56:47 +1000

    UI changes

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -4,7 +4,9 @@ const generateToken = (user) => {
   return jwt.sign(
     { id: user._id, role: user.role || 'user' , name: user.name }, // Include role if available
     process.env.JWT_SECRET || 'yoursecretkey', // Use dotenv in production!
-    { expiresIn: process.env.JWT_EXPIRES_IN || '1h' } // Default to 1 hour if not set
+    {
+      expiresIn: process.env.JWT_EXPIRATION || '15m'
+    } // Default to 1 hour if not set
   );
 };
 
